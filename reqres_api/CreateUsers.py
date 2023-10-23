92d84d8f4f9c3fa5f70a515f269a15c26a085d64
triveni.bika <triveni.bika@capgemini.com>
2023-10-23 14:31:14 +0200

    updated get and post calls

--- reqres_api/CreateUsers.py
+++ reqres_api/CreateUsers.py
@@ -1,28 +1,23 @@
 import requests
 import json
 
-APIURL = 'https://reqres.in'
-create_user = '/api/users'
-
-req_hearders = {
-    'Content-Type': 'application/json'
-}
-
-singleuser_body = {
-    "name": "morpheus",
-    "job": "leader"
-}
+testinput = "C:/TestQA/Twilight/rf-2023/APICourse/reqres_api/usersdata.json"
+with open(testinput, 'r') as getinput:
+    testdata = getinput.read()
+    fetch_data = json.loads(testdata)
 
 
 def post_createUser():
-    response = requests.post(APIURL+create_user, json=singleuser_body, headers=req_hearders)
+    response = requests.post(fetch_data["APIURL"] + fetch_data["create_user"], json=fetch_data["singleuser_body"],
+                             headers=fetch_data["req_hearders"])
     assert response.status_code == 201
-
     req_response = response.json()
     req_jsondata = json.dumps(req_response)
     jsondata = json.loads(req_jsondata)
-
     assert "morpheus" == jsondata["name"]
 
 
 post_createUser()
+
+# close external json file
+getinput.close()
--- reqres_api/GetUsers.py
+++ reqres_api/GetUsers.py
@@ -4,38 +4,35 @@ import requests
 import json
 
 testinput = "C:/TestQA/Twilight/rf-2023/APICourse/reqres_api/usersdata.json"
+with open(testinput, 'r') as getinput:
+    testdata = getinput.read()
+    fetch_data = json.loads(testdata)
 
 
 # GET ALL users
 def get_AllUsers():
-    with open(testinput, 'r') as getinput:
-        testdata = getinput.read()
-        fetch_data = json.loads(testdata)
-        response = requests.get(fetch_data["APIURL"] + fetch_data["get_users"], headers=fetch_data["req_hearders"])
-        # verify status code
-        assert response.status_code == 200
-        req_response = response.json()
+    response = requests.get(fetch_data["APIURL"] + fetch_data["get_users"], headers=fetch_data["req_hearders"])
+    # verify status code
+    assert response.status_code == 200
+    req_response = response.json()
 
-        # <pretty response JSON format>
-        res_jsondata = json.dumps(req_response, indent=4)
-        jsondata = json.loads(res_jsondata)
-        usersDict = {}
-        usersList = []
-        for data in jsondata["data"]:
-            if "Lindsay" in data["first_name"]:
-                print(f'user "{"Lindsay"}" present in json data')
-                assert "Lindsay" == data["first_name"]
-            usersDict[data["id"]] = data["first_name"]
-            usersList.append(data["first_name"])
-
-        #print(f'Store subset of users in dict:{usersDict} ,\\n subset of users in list:{usersList}')
-        # Ques ask2: how to return more than one value?
-        return usersList,usersDict
+    # <pretty response JSON format>
+    res_jsondata = json.dumps(req_response, indent=4)
+    jsondata = json.loads(res_jsondata)
+    usersDict = {}
+    usersList = []
+    for data in jsondata["data"]:
+        if "Lindsay" in data["first_name"]:
+            print(f'user "{"Lindsay"}" present in json data')
+            assert "Lindsay" == data["first_name"]
+        usersDict[data["id"]] = data["first_name"]
+        usersList.append(data["first_name"])
+    return usersList, usersDict
 
 
 # GET Request
 def get_SingleUser():
-    response = requests.get(APIURL + single_user, headers=req_hearders)
+    response = requests.get(fetch_data["APIURL"] + fetch_data["single_user"], headers=fetch_data["req_hearders"])
     # verify status code
     assert response.status_code == 200
     req_response = response.json()
@@ -44,11 +41,12 @@ def get_SingleUser():
     print(jsondata["data"]["id"])
 
     # validate runtime id with actual id
-    assert jsondata["data"]["id"] == int(id)
+    assert jsondata["data"]["id"] == 2
 
 
 def get_SingleUserNotFound():
-    response = requests.get(APIURL + single_user_notfound, headers=req_hearders)
+    response = requests.get(fetch_data["APIURL"] + fetch_data["single_user_notfound"],
+                            headers=fetch_data["req_hearders"])
     req_response = response.json()
     # print("req_response", req_response)
     # verify status code
@@ -57,7 +55,8 @@ def get_SingleUserNotFound():
 
 
 def get_ListResources():
-    response = requests.get(APIURL + list_resource, headers=req_hearders)
+    response = requests.get(fetch_data["APIURL"] + fetch_data["list_resource"],
+                            headers=fetch_data["req_hearders"])
     # verify status code
     assert response.status_code == 200
     req_response = response.json()
@@ -76,7 +75,8 @@ def get_ListResources():
 
 
 def get_ListSingleResource():
-    response = requests.get(APIURL + list_single_resource, headers=req_hearders)
+    response = requests.get(fetch_data["APIURL"] + fetch_data["list_single_resource"],
+                            headers=fetch_data["req_hearders"])
     # verify status code
     assert response.status_code == 200
     req_response = response.json()
@@ -87,45 +87,24 @@ def get_ListSingleResource():
     assert jsondata["data"]["id"] == 6
 
 
-def get_SingleUserNotFound():
-    response = requests.get(APIURL + list_single_resource_notfound, headers=req_hearders)
+def get_SingleResourceNotFound():
+    response = requests.get(fetch_data["APIURL"] + fetch_data["list_single_resource_notfound"], headers=fetch_data["req_hearders"])
     req_response = response.json()
-
     # verify status code when resource not found
     assert response.status_code == 404
     assert len(req_response) == 0
 
 
-
-a ,b = get_AllUsers()
+a, b = get_AllUsers()
+print("\n ****** GET ALL USERS outcome ***** \n")
 print(a)
 print(b)
 
-# get_SingleUser()
-# get_SingleUserNotFound()
-# get_ListResources()
-# get_ListSingleResource()
-# get_SingleUserNotFound()
+get_SingleUser()
+get_SingleUserNotFound()
+get_ListResources()
+get_ListSingleResource()
+get_SingleResourceNotFound()
 
-# Ques ask3: how to generate report in TA.
- # how to handle and validate type of authorizations
- # how to validate headers
- # how to create test cases / test suites ?
- # can we group test cases based on tags ?
- # run tests / test suites in parallel ?
- # run tests in sequential ?
- # re-run failure tests ?
- # trigger batch script / schedule cronjob.
-
- # [gmail / outlook]
- # Notify results in email ( start of suite and completion of suite) - test results
- # Notify email, email results in attachment.
-
- # How to setup framework in virtual env? (use in python and RF)
- # create GIT repo
- # configure tests in Jenkins and gitLab
- # setip different type of suites in pipeline
-
-
-
-
+# close input urls defined json file
+getinput.close()
